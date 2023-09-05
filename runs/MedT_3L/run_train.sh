e293791bdeec228725eab7af445beb9d6b8e31cf
Jona Schulz <jona.schulz@rwth-aachen.de>
2023-09-05 18:46:03 +0200

    Runs

--- runs/MedT_3L/run_train.sh
+++ runs/MedT_3L/run_train.sh
@@ -3,4 +3,4 @@ cd /work/scratch/schulz/Medical-Transformer
 
 python=/work/scratch/schulz/miniconda3/envs/medt/bin/python
 
-$python /work/scratch/schulz/Medical-Transformer/train.py --train_dataset data_3L/train --val_dataset data_3L/val --direc runs/Medt_Run1 --batch_size 4 --epoch 400 --save_freq 10 --modelname "MedT" --learning_rate 0.001 --imgsize 256 --gray "yes" --n_classes 3
+$python /work/scratch/schulz/Medical-Transformer/train.py --train_dataset data_3L/train --val_dataset data_3L/val --direc runs/Medt_Run1 --batch_size 4 --epoch 400 --save_freq 10 --modelname "MedT" --learning_rate 0.001 --imgsize 256 --gray "yes" --n_classes 4
