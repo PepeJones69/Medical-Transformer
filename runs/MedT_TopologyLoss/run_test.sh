4eadfbaea44232a50859064158435e5f20b9674d
Jona Schulz <jona.schulz@rwth-aachen.de>
2023-09-14 10:37:35 +0200

    Runs

--- runs/MedT_TopologyLoss/run_test.sh
+++ runs/MedT_TopologyLoss/run_test.sh
@@ -3,4 +3,4 @@ cd /work/scratch/schulz/Medical-Transformer
 
 python=/work/scratch/schulz/miniconda3/envs/medt/bin/python
 
-$python /work/scratch/schulz/Medical-Transformer/test.py --loaddirec results/final_model.pth --val_dataset data/val --direc runs/MedT_TopologyLoss --batch_size 4 --modelname "MedT" --imgsize 256 --gray "yes"
+$python /work/scratch/schulz/Medical-Transformer/test.py --loaddirec runs/MedT_TopologyLoss/final_model.pth --val_dataset data/val --direc runs/MedT_TopologyLoss --batch_size 4 --modelname "MedT" --imgsize 256 --gray "yes"
