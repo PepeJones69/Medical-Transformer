3f1c88c7d1525a63c91ce0c83d4b9679e35f1053
Jona Schulz <jona.schulz@rwth-aachen.de>
2023-09-26 11:48:17 +0200

    Runs

--- runs/bilateral/run_test.sh
+++ runs/bilateral/run_test.sh
@@ -3,4 +3,4 @@ cd /work/scratch/schulz/Medical-Transformer
 
 python=/work/scratch/schulz/miniconda3/envs/medt/bin/python
 
-$python /work/scratch/schulz/Medical-Transformer/test.py --loaddirec runs/MedT_Run2/final_model.pth --val_dataset data_new_bilateral --direc runs/bilateral --batch_size 4 --modelname "MedT" --imgsize 256 --gray "yes"
+$python /work/scratch/schulz/Medical-Transformer/test.py --loaddirec runs/MedT_Original/final_model.pth --val_dataset data_new_bilateral --direc runs/bilateral --batch_size 4 --modelname "MedT" --imgsize 256 --gray "yes"
