43be336978ee68cbb3055c2d6f8dd10d26696871
Jona Schulz <jona.schulz@rwth-aachen.de>
2023-09-29 03:47:03 +0200

    rundata

--- runs/MedT_Original/run_train.sh
+++ runs/MedT_Original/run_train.sh
@@ -3,4 +3,4 @@ cd /work/scratch/schulz/Medical-Transformer
 
 python=/work/scratch/schulz/miniconda3/envs/medt/bin/python
 
-$python /work/scratch/schulz/Medical-Transformer/train.py --train_dataset data_original/train --val_dataset data_original/val --direc runs/Medt_Original --batch_size 4 --epoch 400 --save_freq 10 --modelname "MedT" --learning_rate 0.001 --imgsize 256 --crop 256 --gray "yes"
+$python /work/scratch/schulz/Medical-Transformer/train.py --train_dataset data_original/train --val_dataset data_original/val --direc runs/MedT_Original --batch_size 4 --epoch 400 --save_freq 10 --modelname "MedT" --learning_rate 0.001 --imgsize 256 --crop 256 --gray "yes"
